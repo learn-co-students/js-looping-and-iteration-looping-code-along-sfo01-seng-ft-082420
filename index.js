b2c7841bdf19b14909d83106382c3a56267c9825
Will-Gon <will.colocho@gmail.com>
2020-10-05 22:04:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, string) {
+    const names = []
+    for (let i = 0; i < array.length; i++) {
+        names.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+    return names
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+}
